8b89d04a2a2078e4ae9ca3ac66c582778c95802f
williammanning <billm@billm-mac.local>
2017-05-24 00:51:42 -0700

    Dockerfile and prep changes

--- bintray/prep.sh
+++ bintray/prep.sh
@@ -1,8 +1,8 @@
 cp /usr/local/bin/jfrog .
 cd bintrayrocks
-tar -cvf amaze-1.0.tgz exec
+tar -cvf amaze-1.0-x86.tgz exec
 cd ..
-cp bintrayrocks/amaze-1.0.tgz .
+cp bintrayrocks/amaze-1.0-x86.tgz .
 jfrog rt u amaze-1.0.tgz jfrog.local/jfrog/bintray/bintray-webinar/amaze/
 docker build -t jfrog.local:5001/amaze:1.0 .
 docker push jfrog.local:5001/amaze:1.0
