d70bbb3bb0f22df481db48302e2ad3b2b29fcdac
williammanning <billm@billm-mac.local>
2017-05-24 08:11:58 -0700

    jfrog.local

--- bintray/prep.sh
+++ bintray/prep.sh
@@ -3,6 +3,6 @@ cd bintrayrocks
 tar -cvf amaze-1.0-x86.tgz exec
 cd ..
 cp bintrayrocks/amaze-1.0-x86.tgz .
-jfrog rt u amaze-1.0.tgz 35.188.114.255:8084/jfrog/bintray/bintray-webinar/amaze/
-docker build -t 35.188.114.255:8084/amaze:1.0 .
-docker push 35.188.114.255:8084/amaze:1.0
+jfrog rt u amaze-1.0.tgz jfrog.local/jfrog/bintray/bintray-webinar/amaze/
+docker build -t jfrog.local:5001/amaze:1.0 .
+docker push jfrog.local:5001/amaze:1.0
