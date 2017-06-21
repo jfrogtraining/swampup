13a6a9d4c30feb16b284272d7889e0e981849780
Stanley Fong <stanleyf@jfrog.com>
2017-06-21 14:51:06 -0700

    fixed build number issue

--- src/org/jfrog/MyArtifactory.groovy
+++ src/org/jfrog/MyArtifactory.groovy
@@ -6,7 +6,7 @@ class MyArtifactory implements Serializable {
     def buildInfo
     def rtMaven
 
-    MyArtifactory (artifactory, bNumber) {
+    MyArtifactory (def artifactory) {
         this.server = artifactory.server 'artifactory-ha'
         this.rtMaven = artifactory.newMavenBuild()
         this.buildInfo = artifactory.newBuildInfo()
@@ -22,19 +22,19 @@ class MyArtifactory implements Serializable {
         this.rtMaven.run pom: 'maven-example/pom.xml', goals: 'clean install', buildInfo: this.buildInfo
     }
 
-    def upLoadToArtifactory () {
+    def upLoadToArtifactory (def buildNo) {
         def uploadSpec = """{
             "files": [
                {
                     "pattern": "/var/lib/jenkins/workspace/HAP-935/(*).zip",
-                    "target": "hap-935a/tibco/bNumber/{1}.zip",
+                    "target": "hap-935a/tibco/${buildNo}/{1}.zip",
                     "props": "hap-935=true",
                     "flat": "false",
                     "regexp":"false"
                 },
                 {
                     "pattern" : "(*)pom.xml",
-                    "target":"hap-935a/tibco/bNumber/{1}.xml",
+                    "target":"hap-935a/tibco/${buildNo}/{1}.xml",
                     "flat":"false",
                     "props":"type=pom;status=ready"
                 }
