2565cb113a4efb5d81ebd46f047da4c94b64cd6b
memsharded <james@conan.io>
2018-04-29 22:41:54 +0200

    updating gtest example

--- conan/exercises/gtest/package/conanfile.py
+++ conan/exercises/gtest/package/conanfile.py
@@ -1,27 +1,19 @@
-import os
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
 
 
 class HelloConan(ConanFile):
-    name = "hello"
-    version = "1.0"
+    name = "Hello"
+    version = "0.1"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     exports_sources = "*"
-    requires = "gtest/1.8.0@lasote/stable"
+    requires = "gtest/1.8.0@bincrafters/stable"
     default_options = "gtest:shared=False"
 
     def build(self):
         cmake = CMake(self)
-
-        if os.environ.get("TEST_ENABLED") == "1":
-            cmake.definitions["TEST_ENABLED"] = "1"
-
         cmake.configure()
         cmake.build()
-
-        if os.environ.get("TEST_ENABLED") == "1":
-            self.run("bin/runUnitTests")
 
     def package(self):
         self.copy("*.h", dst="include")
